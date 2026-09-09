c74b71f237c3c979bca97355146d8804b8d045f4
Priyanshu Kumar <pk2382097@gmail.com>
2026-09-09 23:19:15 +0530

    update

---
+++ differ.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 25;
+    int b = 10;
+
+    int difference = a - b;
+
+    cout << "Difference = " << difference;
+
+    return 0;
+}
